f265b529847ab7b4771b439935374fbb561e6083
piaoransheng <421946786@qq.com>
2020-06-29 15:42:35 +0800

    去掉没用的包导入

--- src/main/java/com/king/proxy/cglib/ProxyFactory.java
+++ src/main/java/com/king/proxy/cglib/ProxyFactory.java
@@ -3,10 +3,7 @@ package com.king.proxy.cglib;
 import org.springframework.cglib.proxy.Enhancer;
 import org.springframework.cglib.proxy.MethodInterceptor;
 import org.springframework.cglib.proxy.MethodProxy;
-
-import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
-import java.lang.reflect.Proxy;
 
 public class ProxyFactory implements MethodInterceptor {
 
