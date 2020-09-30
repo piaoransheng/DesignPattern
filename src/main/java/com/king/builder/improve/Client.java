f68466fe23be6473e66421f2c9e92288eb418963
piaoransheng <421946786@qq.com>
2020-09-30 09:41:44 +0800

    去除黄色警告

--- src/main/java/com/king/builder/improve/Client.java
+++ src/main/java/com/king/builder/improve/Client.java
@@ -3,9 +3,9 @@ package com.king.builder.improve;
 public class Client {
     public static void main(String[] args) {
         //准备盖房子的技术人员
-        CommonHouseBulider commonHouseBulider = new CommonHouseBulider();
+        HouseBuilder houseBulider = new HighHouseBulider();
         //准备盖房子的指挥者
-        HouseDirector houseDirector = new HouseDirector(commonHouseBulider);
+        HouseDirector houseDirector = new HouseDirector(houseBulider);
         House house = houseDirector.buildHouse();
     }
 }
