a887387c26de077a62e2eddb2196d19f9bbd0844
piaoransheng <421946786@qq.com>
2020-12-30 13:22:11 +0800

    依赖倒转原则注释

--- src/main/java/com/priciple/inversion/common/Client.java
+++ src/main/java/com/priciple/inversion/common/Client.java
@@ -1,5 +1,10 @@
 package com.priciple.inversion.common;
 
+/**
+ * 没有使用依赖倒转原则
+ *
+ * 缺点：如果要读微信信息，person类就得再加一个方法；使用接口，读微信信息person类就不用再加方法，只要价格微信消息类继承消息接口
+ */
 public class Client {
     public static void main(String[] args) {
         Person person = new Person();
--- src/main/java/com/priciple/inversion/improve/Client2.java
+++ src/main/java/com/priciple/inversion/improve/Client2.java
@@ -3,6 +3,10 @@ package com.priciple.inversion.improve;
 import com.priciple.inversion.improve.message.EmailMessage;
 import com.priciple.inversion.improve.message.WechatMessage;
 
+/**
+ * 使用依赖倒转原则：使用接口或者抽象类
+ * 新增消息类型不用在person类再加readMessage方法，只需要再加一个消息实现Message接口
+ */
 public class Client2 {
     public static void main(String[] args) {
         Person2 person2 = new Person2();
